32736b6b3a1c605d24d117714f080b33b50e83c5
7szho <tszhotang77@gmail.com>
2025-07-14 02:32:13 +0800

    feat(models): Implement initial job and log models

--- common/models/job_proc.go
+++ common/models/job_proc.go
@@ -6,21 +6,24 @@ import (
 	"time"
 )
 
+// JobProcVal 存储作业进程的状态信息
 type JobProcVal struct {
 	Time   time.Time `json:"time"`    // 开启执行时间
 	Killed bool      `json:"Kiddled"` // 是否强制杀死
 }
 
+// JobProc 表示一个作业进程，包括其基本信息和状态
 type JobProc struct {
-	ID       int    `json:"id"`
-	JobID    int    `json:"job_id"`
-	NodeUUID string `json:"node_uuid"`
-	JobProcVal
+	ID         int    `json:"id"`        // 作业进程ID
+	JobID      int    `json:"job_id"`    // 关联的作业ID
+	NodeUUID   string `json:"node_uuid"` // 节点唯一标识
+	JobProcVal        // 作业进程状态
 
-	Running int32
-	Wg      sync.WaitGroup
+	Running int32          // 运行状态标识
+	Wg      sync.WaitGroup // 用于同步的等待组
 }
 
+// Val 返回 JobProcVal 的 JSON 字符串表示
 func (p *JobProc) Val() (string, error) {
 	b, err := json.Marshal(&p.JobProcVal)
 	if err != nil {
